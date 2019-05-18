8695ce67c3a78be23cf4206603fdfaa3dc860057
Otho Jessi <ojessi@at-e1.21-school.ru>
2019-05-18 20:13:51 +0300

    help

---
+++ libfta/ft_list_remove_free_if.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_free_if.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ojessi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/18 18:43:17 by ojessi            #+#    #+#             */
+/*   Updated: 2019/04/18 21:49:20 by ojessi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+static	void		ft_free(t_list *list)
+{
+	free(list->content);
+	list->content = NULL;
+	free(list);
+	list = NULL;
+}
+
+void				ft_list_remove_free_if(t_list **begin_list, int elem)
+{
+	t_list			*list;
+	t_list			*tmp;
+
+	list = *begin_list;
+	if ((int)list->content_size == elem)
+	{
+		*begin_list = list->next;
+		ft_free(list);
+		return ;
+	}
+	tmp = list;
+	while (list)
+	{
+		if ((int)list->content_size == elem)
+		{
+			tmp->next = list->next;
+			ft_free(list);
+			return ;
+		}
+		tmp = list;
+		list = list->next;
+	}
+}
